2965b811071b380734ec5f3d07019508e0173278
TahaniAsASuper <134502059+TahaniAsASuper@users.noreply.github.com>
2023-06-08 11:15:57 +0300

    Initial commit

---
+++ src/assets/js/products.js
@@ -0,0 +1,24 @@
+import BasePage from './base-page';
+
+class Products extends BasePage {
+    onReady() {
+        let urlParams = new URLSearchParams(window.location.search);
+        if (urlParams.has('sort')) {
+            app.element('#product-filter').value = urlParams.get('sort');
+        }
+        // Sort Products
+        app.on('change','#product-filter', async event =>{
+            window.history.replaceState(null, null, salla.helpers.addParamToUrl('sort',event.currentTarget.value));
+            app.element('salla-products-list').sortBy=event.currentTarget.value;
+            await app.element('salla-products-list').reload();
+            app.element('salla-products-list').setAttribute('filters', `{"sort": "${event.currentTarget.value}"}`)
+        });
+    }
+}
+
+Products.initiateWhenReady([
+    'product.index',
+    'product.index.latest',
+    'product.index.offers', 'product.index.search',
+    'product.index.tag',
+]);
